6a91eac70fabe4a63abb2918e36b34a7548d7d80
Cole Sellers <sellersc@arch04.cis.gvsu.edu>
2019-09-05 17:03:48 -0400

    made it so that i recieve all k,v pairs

--- BoomTownTest.py
+++ BoomTownTest.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 url = "https://api.github.com/orgs/boomtownroi"
-string = "api.github.com/orgs/BoomTownROI"
+substring = "api.github.com/orgs/BoomTownROI"
 
 def main():
     response = requests.get(url)
@@ -11,26 +11,29 @@ def main():
 
     for key in body:
         value = body[key]
-        if str(value).find(string) != -1:
+        if str(value).find(substring) != -1:
             response = requests.get(value)
             if response.status_code == 200: 
                 page = json.loads(response.content)
                 checkForID(page)
-        else:
-            print("whoops")
+            else: 
+                print("error page does not exist")
+    if 
 
-def checkForID(jsonlist):
-    for i in jsonlist:
-        print('here')
-        if i == "id":
-            print(i + ':' + str(jsonlist[i]))
-        elif isinstance(i, dict):
-            #for j in i:
-                #if j == "id":
-                #    print(j + ':' +str(i[j]))
+
+def checkForID(page):
+    if isinstance(page, dict):
+        for k,v in page.items():
+            if isinstance(v, (dict,list)):
+                checkForID(v)
+            elif k == "id":
+                print(k +" : " + str(v))
+    elif isinstance(page, list):
+        for i in page: 
             checkForID(i)
+            
+
 
 
 if __name__ == '__main__':
     main()
-
